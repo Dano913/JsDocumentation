1b1303f7b493c132a4dc4dd6e9a9ab6cbeafdbca
Dano913 <daniaxve913@gmail.com>
2025-12-09 23:26:58 +0100

    Más apuntes

--- js/funciones/navbar.js
+++ js/funciones/navbar.js
@@ -1,60 +1,60 @@
-export function runJsEditor() {
-  const editor = document.getElementById("editor");
-  const preview = document.getElementById("preview");
+export function runJsEditor() {                                 // Abre el editor de js
+  const editor = document.getElementById("editor");             // Rescato html de ruleta.html
+  const preview = document.getElementById("preview");           // Rescato html de ruleta.html
 
-  if (!editor || !preview) return;
+  if (!editor || !preview) return;                              // Si no existen no se ejecuta nada
 
-  function render() {
-    const js = editor.innerText;
-    const iframeWindow = preview.contentWindow;
+  function render() {                                           // Funcion de renderizar el editor
+    const js = editor.innerText;                                // Obtiene el texto del editor
 
-    const blob = new Blob([js], { type: "text/javascript" });
-    const blobURL = URL.createObjectURL(blob);
+    const blob = new Blob([js], { type: "text/javascript" });   // Guarda el codigo js de manera temporal
+    const blobURL = URL.createObjectURL(blob);                  // Se crea una url que apunta a ese archivo temporal
 
-    preview.srcdoc = `<!DOCTYPE html>
+    preview.srcdoc = `<!DOCTYPE html>           
       <html>
         <body>
           <script src="${blobURL}"></script>
         </body>
       </html>`
-    ;
-  }
+    ;                 // srdoc reemplaza el contenido del iframe con un HTML
+                      // Este HTML carga el script generado por el usuario. 
+  }                   // El codigo js se ejecuta aisladamente sin afectar a lo de fuera.
 
-  render();
-  editor.addEventListener("input", render);
+  render();                                   // LLamo a la funcion para que se ejecute el codigo
+  editor.addEventListener("input", render);   // Reejecuta cuando actualizo el codigo
 }
 
-export function setupThemeToggle(themeToggle, body) {
-  if (!themeToggle) return;
+export function setupThemeToggle(themeToggle, body) {               // Funcion del themetoggle
+  if (!themeToggle) return;                                         // Si no existe termina 
 
-  if (localStorage.getItem("theme") === "dark") {
-    body.classList.add("dark-theme");
-    themeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+  if (localStorage.getItem("theme") === "dark") {                   // Si el tema esta guardado en el localstorage 
+    body.classList.add("dark-theme");                               // Lo pone
+    themeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';      // Cambia el icono a sol
   }
 
-  themeToggle.addEventListener("click", () => {
-    body.classList.toggle("dark-theme");
-    if (body.classList.contains("dark-theme")) {
-      themeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
-      localStorage.setItem("theme", "dark");
+  themeToggle.addEventListener("click", () => {                     // Escucha el evento click en el boton de themetoggle
+    body.classList.toggle("dark-theme");                            // Activa o desactiva el darktheme
+    if (body.classList.contains("dark-theme")) {                    
+      themeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';    // Coloca el sol
+      localStorage.setItem("theme", "dark");                        // Guarda el tema
     } else {
-      themeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
-      localStorage.setItem("theme", "light");
+      themeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';   // Coloca la luna
+      localStorage.setItem("theme", "light");                       // Guarda el tema
     }
   });
 }
 
-export function setupExternalEditorButton() {
-  const externalBtn = document.getElementById("external-window-btn");
-  if (!externalBtn) {
+export function setupExternalEditorButton() {                          // COnfigura el boton del editor
+  const externalBtn = document.getElementById("external-window-btn");  // Rescato el html
+  if (!externalBtn) {                                                  // Si no existe termina
     console.error("⚠️ No se encontró el botón external-window-btn");
     return;
   }
-  externalBtn.addEventListener("click", () => {
-    window.open(
-      "./components/editor/editor.html",
-      "VentanaEditor",
-      "width=1000,height=700,resizable=yes,scrollbars=yes"
+  externalBtn.addEventListener("click", () => {                        // Escucha el click
+    window.open(                                                       // Abre una ventana
+      "./components/editor/editor.html",                               // Con el html
+      "VentanaEditor",                                                 // Si ya existe se reutiliza la ventana
+      "width=1000,height=700,resizable=yes,scrollbars=yes"             // Estilos
     );
   });
 }
--- js/funciones/sidebar.js
+++ js/funciones/sidebar.js
@@ -1,32 +1,32 @@
 import { loadPage } from './body.js';
 
-export function setupSidebarLinks(sidebar, main) {
-  const sidebarLinks = sidebar.querySelectorAll('a');
-  sidebarLinks.forEach(link => {
-    link.addEventListener('click', e => {
-      e.preventDefault();
-      sidebarLinks.forEach(l => l.classList.remove('active'));
-      link.classList.add('active');
+export function setupSidebarLinks(sidebar, main) {        // Funcion para colocar los links con parametros
+  const sidebarLinks = sidebar.querySelectorAll('a');     // Los guardar en un variable
+  sidebarLinks.forEach(link => {                          // Recorro los links
+    link.addEventListener('click', e => {                       // Añado evento de click a cada uno
+      e.preventDefault();                                       // Evita comportamiento natural
+      sidebarLinks.forEach(l => l.classList.remove('active'));  // Elimina la clase active de todos
+      link.classList.add('active');                             // Se añade la clase active solo al clickado
       
-      const href = link.getAttribute('href');
-      if (href.startsWith('#')) {
-        const target = document.querySelector(href);
-        if (target) target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      const href = link.getAttribute('href');            // Obtiene el valor de href del link clickado
+      if (href.startsWith('#')) {                        // Comprueba si empieza con #(ID dentro de la pagina)
+        const target = document.querySelector(href);     // Guarda en variable
+        if (target) target.scrollIntoView({ behavior: 'smooth', block: 'start' }); // Busca el que coincida con el valor de href que guardo
       } else {
-        loadPage(href, main);
+        loadPage(href, main);    // Sino es que es una pagina por eso la carga SPA
       }
     });
   });
 }
 
-export function setupMenuToggle(menuBtn, sidebar, pageWrapper) {
-  menuBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('active');
+export function setupMenuToggle(menuBtn, sidebar, pageWrapper) { // Boton de plegar y desplegar sidebar
+  menuBtn.addEventListener('click', () => {                      // Se escucha el evento de click
+    sidebar.classList.toggle('active');                          // Si no tiene la clase active se la pone y viceversa 
 
-    if (sidebar.classList.contains('active')) {
-      pageWrapper.style.marginLeft = '250px';
+    if (sidebar.classList.contains('active')) {     // Si la sidebar esta abierta
+      pageWrapper.style.marginLeft = '250px';       // Mueve el contenido
     } else {
-      pageWrapper.style.marginLeft = '0';
+      pageWrapper.style.marginLeft = '0';           // Sino lo deja quieto
     }
   });
 }
